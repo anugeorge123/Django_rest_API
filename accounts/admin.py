a43d9521e2e0de6ab03c2afb11c5a697c165063a
Anu <anumanackanappillil@gmail.com>
2020-06-15 09:29:18 +0530

    message

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from accounts.models import Country, State, City, User, UserRole
+from accounts.models import Country, State, City, User, UserRole,Message
 # from django.contrib.auth.admin import UserAdmin
 
 # class UserAdmin(UserAdmin):
@@ -11,3 +11,4 @@ admin.site.register(State, admin.ModelAdmin)
 admin.site.register(City, admin.ModelAdmin)
 admin.site.register(User, admin.ModelAdmin)
 admin.site.register(UserRole, admin.ModelAdmin)
+admin.site.register(Message, admin.ModelAdmin)
